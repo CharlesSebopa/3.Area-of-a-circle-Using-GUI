ec257cca4b8898c45233da6a74115a86d441b5a1
Lenovo-User <Lenovo-User@DESKTOP-7L4SHKG>
2024-01-25 06:55:06 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.Area_of_a_circle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Area, radius;
+
+            if (double.TryParse(textBox1.Text, out radius)) 
+            {
+                Area = Math.PI * Math.Pow(radius, 2);
+
+                   textBox2.Text = Area.ToString("F2");
+            }
+            else
+            { MessageBox.Show("Please enter a valid number for the radius.", "Error"); }
+        }
+    }
+}
